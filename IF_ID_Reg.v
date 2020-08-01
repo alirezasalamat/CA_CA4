f74c576a3bc2fb2cc4cdc6acf2c0706c89633bdc
Alireza Salamat <52575958+alirezasalamat@users.noreply.github.com>
2020-08-01 19:02:06 +0430

    little changes

--- IF_ID_Reg.v
+++ IF_ID_Reg.v
@@ -1,12 +1,12 @@
-module IF_ID_Reg(clk , flush , stall , instruction_in , 
+module IF_ID_Reg(clk , flush , IF_ID_write , instruction_in , 
                     pc_in , instruction_out , pc_out);
                     
-    input clk , flush , stall;
+    input clk , flush , IF_ID_write;
     input [31:0] instruction_in , pc_in;
     output reg [31:0] instruction_out, pc_out;
 
     always @(posedge clk) begin
-        if(stall == 1'b0) begin
+        if(IF_ID_write == 1'b1) begin
             if(flush == 1'b1)begin
                 instruction_out <= 32'bz;
                 pc_out <= pc_in;
